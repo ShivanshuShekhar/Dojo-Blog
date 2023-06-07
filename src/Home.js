3b64e21ffbe6d1aceb82bc514cf0008d5c1a43dc
Shivanshu Shekhar <shivanshushekhar786@gmail.com>
2023-06-07 12:00:06 +0530

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hello Ninjas');
+    };
+
+    const handleClickAgain = (name) => {
+        console.log('Hello ' + name);
+    };
+
     return (
         <div className="name">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClickAgain('mario')}>Click Me Again</button>
         </div>
     );
 }
