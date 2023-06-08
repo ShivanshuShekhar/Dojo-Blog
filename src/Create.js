2bd1b2799e8937501e19019767394034acd14b38
Shivanshu Shekhar <shivanshushekhar786@gmail.com>
2023-06-08 12:14:03 +0530

    Programmatic Redirects

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
 
@@ -6,6 +7,7 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('yoshi');
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault(); // Prevents page refresh
@@ -20,7 +22,10 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added');
             setIsPending(false);
-        })
+            // history.go(-1);
+            history.push('/');
+        });
+
     }
 
     return (
